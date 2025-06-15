7b2f97d0649e9ecbf0e12e8ff72fded69897c333
Richard Chen <yingchuan.chen.2007@gmail.com>
2025-06-15 20:07:08 +0800

    fix: tmpl highlight

--- dot_config/nvim/lua/config/autocmds.lua
+++ dot_config/nvim/lua/config/autocmds.lua
@@ -3,11 +3,12 @@
 -- Add any additional autocmds here
 
 vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
-  pattern = "*.gohtml,*.gotmpl,*.html,*.tmpl",
-  callback = function()
-    if vim.fn.search("{{.\\+}}", "nw") ~= 0 then
-      local buf = vim.api.nvim_get_current_buf()
-      vim.api.nvim_buf_set_option(buf, "filetype", "gotmpl")
-    end
-  end,
+	pattern = { "*.gohtml", "*.gotmpl", "*.html", "*.tmpl" },
+	callback = function()
+		if vim.fn.search("{{.\\+}}", "nw") ~= 0 then
+			local buf = vim.api.nvim_get_current_buf()
+			vim.api.nvim_buf_set_option(buf, "filetype", "gotmpl")
+		end
+		vim.bo.filetype = "gotmpl"
+	end,
 })
