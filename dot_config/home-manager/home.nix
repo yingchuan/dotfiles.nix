968cf523ee7c02b25b336157add86de631d7a130
Richard Chen <yingchuan.chen.2007@gmail.com>
2025-06-15 15:08:01 +0800

    fix: nvim nodejs issue

--- dot_config/home-manager/home.nix
+++ dot_config/home-manager/home.nix
@@ -30,6 +30,7 @@
     tmux         # terminal multiplexer
     lua          # latest stable (currently 5.4)
     gcc          # GNU C/C++ compiler (gcc + g++)
+    nodejs       # JavaScript/TypeScript runtime (includes npm)
     # # Adds the 'hello' command to your environment. It prints a friendly
     # # "Hello, world!" when run.
     # pkgs.hello
