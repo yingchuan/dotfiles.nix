b9669f8012cc66ae620314a9f46ad9193f8769f2
Richard Chen <yingchuan.chen.2007@gmail.com>
2025-06-22 15:43:12 +0800

    fix: fcitx bugfix

--- dot_config/home-manager/home.nix
+++ dot_config/home-manager/home.nix
@@ -145,7 +145,7 @@ in
   programs.home-manager.enable = true;
 
   # ── Input-method ─────────────────────────────────────────────
-  services.fcitx5 = {
+  programs.fcitx5 = {
     enable = true;                       # start fcitx5 + export env-vars
     addons = with pkgs; [
       fcitx5-chinese-addons              # Pinyin, handwriting, etc.
