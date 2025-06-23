5672f62dcbd78359b7668d3f03756e4a81a24a81
Richard Chen <yingchuan.chen.2007@gmail.com>
2025-06-23 10:04:20 +0800

    fix: add nvim config to resolve ssh remote clipboard failed

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -3,6 +3,7 @@
 -- Add any additional options here
 vim.g.lazyvim_picker = "snacks"
 vim.g.clipboard = "osc52"
+vim.opt.clipboard:append({ "unnamed", "unnamedplus" })
 -- LSP Server to use for Python.
 -- Set to "basedpyright" to use basedpyright instead of pyright.
 vim.g.lazyvim_python_lsp = "ruff"
